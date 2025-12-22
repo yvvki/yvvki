e17a5a89a8983d707ccf0e4a5d88347e540a1c10
Yuuki Rika <yvvki@kekkon.nexus>
2025-12-22 20:17:22 +0700

    build: setup client and tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,34 @@
-import ssg from "@hono/vite-ssg";
 import honox, { devServerDefaultOptions } from "honox/vite";
+import client from "honox/vite/client";
+import ssg from "@hono/vite-ssg";
+
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 const entry = "./app/server.ts";
+const input = ["./app/client.ts", "./app/style.css"];
 
-export default defineConfig({
+const clientConfig = defineConfig({
+	plugins: [client({ input }), tailwindcss()],
+});
+
+const serverConfig = defineConfig({
 	plugins: [
 		honox({
 			entry,
 			devServer: {
 				exclude: [...devServerDefaultOptions.exclude, /^\/public\/.+/],
 			},
+			client: { input },
 		}),
+		tailwindcss(),
 		ssg({ entry }),
 	],
+	build: {
+		emptyOutDir: false,
+	},
 });
+
+export default defineConfig(({ mode }) =>
+	mode === "client" ? clientConfig : serverConfig,
+);
